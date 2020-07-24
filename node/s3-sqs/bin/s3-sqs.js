c41cf5c0a8ebe2e104db5e51f8829e26f79ca31b
Roman Naumenko <roman+git@naumenko.ca>
2020-07-24 08:14:04 -0400

    Multi-stack for different environments

--- node/s3-sqs/bin/s3-sqs.js
+++ node/s3-sqs/bin/s3-sqs.js
@@ -4,7 +4,14 @@ const cdk = require('@aws-cdk/core');
 const { S3SqsStack } = require('../lib/s3-sqs-stack');
 
 const app = new cdk.App();
-new S3SqsStack(app, 'S3SqsStack', {
+
+new S3SqsStack(app, 'S3SqsStackDev', {
+    env : {
+        region : 'us-east-1'
+    }
+});
+
+new S3SqsStack(app, 'S3SqsStackTest', {
     env : {
         region : 'us-east-1'
     }
