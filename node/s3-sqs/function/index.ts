8817f9e668b1be23f88242db69936ba843b5c0c1
Roman Naumenko <roman+git@naumenko.ca>
2020-12-03 14:31:26 -0500

    update function

--- node/s3-sqs/function/index.ts
+++ node/s3-sqs/function/index.ts
@@ -1,7 +1,7 @@
-import { Context, SQSEvent } from "aws-lambda";
+import { Context, SQSEvent } from 'aws-lambda';
 
 export const handler = async (event: SQSEvent, context: Context): Promise<any> => {
-    console.log("EVENT=%s",JSON.stringify(event));
+    console.log("EVENT=%s",JSON.stringify(event), context);
     return "test response";
         
 }
